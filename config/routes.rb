c0547fcebadb1920d01669021edfb7b80631908a
keyannam <keyannamayfield@gmail.com>
2016-03-12 07:56:30 -0600

    Created Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" } 
+  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
+
+  match '/my_current_user' => 'users#my_current_user', via: [:get]
+
+  match '/random_users' => 'users#random_users', via: [:get]
+
+  match 'users/:id'    => 'users#show_current_user_profile', via: [:get]
+
+  match 'users/:id'    => 'users#update_user', via: [:patch]
+
+  match '/users/profile/:username' => 'users#show_user_profile', via: [:get]
+
+  get '/follow' => "users#follow"
+  get '/unfollow' => "users#unfollow"
+
+  get '/followers/:id'   =>  "users#followers"
+  get '/following/:id'   =>  "users#following"
+
+  get '/users/:user_id/is_following' => "users#is_following"
+
 
 end
