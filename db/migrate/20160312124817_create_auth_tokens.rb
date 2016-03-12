d91305c66f497ac8832a7a5a3dbd4f89666d8c19
keyannam <keyannamayfield@gmail.com>
2016-03-12 07:30:17 -0600

    Initial Commit

---
+++ db/migrate/20160312124817_create_auth_tokens.rb
@@ -0,0 +1,13 @@
+class CreateAuthTokens < ActiveRecord::Migration
+  def change
+    create_table :auth_tokens do |t|
+      t.string :body
+      t.references :user, index: true, foreign_key: true
+      t.datetime :last_used_at
+      t.string :ip_address
+      t.string :user_agent
+
+      t.timestamps null: false
+    end
+  end
+end
